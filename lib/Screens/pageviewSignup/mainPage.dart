2a80f60db0848ab4bdc08d387ff5bc1d72612063
ayasuwadeh <ayafrhn@gmail.com>
2021-02-20 14:39:18 +0200

    before adding indicator

---
+++ lib/Screens/pageviewSignup/mainPage.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class SignUpPageView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class SignUpView extends StatefulWidget {
+  @override
+  _SignUpViewState createState() => _SignUpViewState();
+}
+
+class _SignUpViewState extends State<SignUpView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+
