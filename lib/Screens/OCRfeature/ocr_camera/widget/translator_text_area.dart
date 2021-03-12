a209bf0bff968e8a83b19b4dff3e271eef363f65
ayasuwadeh <ayafrhn@gmail.com>
2021-03-12 02:07:00 +0200

    adding ocr successfully

--- lib/Screens/OCRfeature/ocr_camera/widget/translator_text_area.dart
+++ lib/Screens/OCRfeature/ocr_camera/widget/translator_text_area.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:clipboard/clipboard.dart';
 
 class TextAreaWidget extends StatelessWidget {
   final String text;
-  final VoidCallback onClickedCopy;
+  //final VoidCallback onClickedCopy;
 
   const TextAreaWidget({
     @required this.text,
-    @required this.onClickedCopy,
+    //@required this.onClickedCopy,
     Key key,
   }) : super(key: key);
 
@@ -43,7 +44,7 @@ class TextAreaWidget extends StatelessWidget {
               child: IconButton(
                 icon: Icon(Icons.copy, color: Colors.black),
                 color: Colors.grey[200],
-                onPressed: onClickedCopy,
+                onPressed: copyToClipboard,
               ),
             ),
 
@@ -52,6 +53,11 @@ class TextAreaWidget extends StatelessWidget {
         // const SizedBox(width: 8),
       ],
     );
+  }
+  void copyToClipboard() {
+    if (text.trim() != '') {
+      FlutterClipboard.copy(text);
+    }
   }
 
 }
