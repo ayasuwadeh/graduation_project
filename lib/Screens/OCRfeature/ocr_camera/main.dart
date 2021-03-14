550efb86a49d7f86a41a8d503cc46b591c56291f
ayasuwadeh <ayafrhn@gmail.com>
2021-03-14 19:19:54 +0200

    adding ocr successfully

--- lib/Screens/OCRfeature/ocr_camera/main.dart
+++ lib/Screens/OCRfeature/ocr_camera/main.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_mobile_vision/flutter_mobile_vision.dart';
 import 'package:graduation_project/Screens/OCRfeature/ocr_camera/widget/text_area_widget.dart';
 import 'package:clipboard/clipboard.dart';
-
+import 'package:permission_handler/permission_handler.dart';
+import 'dart:io';
 import 'dart:async';
 
 
@@ -15,15 +16,29 @@ class _MainCameraState extends State<MainCamera> {
   bool _isInitialized=false;
   String obtainedText='Result of scanning';
 
+  PermissionStatus _status;
+
   @override
   void initState() {
-    FlutterMobileVision.start().then((value) {
-      _isInitialized=true;
-    });
     super.initState();
+    //Permission.photos.status.then((value) => updateStatus(value));
+  }
+
+  updateStatus(PermissionStatus status) {
+    if (status != _status) {
+      setState(() {
+        _status = status;
+      });
+    }
   }
 
   _startScan() async{
+    Permission.camera.status.then((value) => updateStatus(value));
+    askLocationPermission();
+    FlutterMobileVision.start().then((value) {
+      _isInitialized=true;
+    });
+
     List <OcrText> tokens=List();
   obtainedText='';
 
@@ -97,5 +112,61 @@ class _MainCameraState extends State<MainCamera> {
        FlutterClipboard.copy(obtainedText);
      }
    }
+  void askLocationPermission() {
+    Permission.camera.request().then((status) => whenRequested(status));
+  }
+
+  whenRequested(PermissionStatus status) {
+    if (status.isDenied) {
+      showLocationIsNeededDialog(true);
+    } else if (Platform.isAndroid && status.isPermanentlyDenied) {
+      showLocationIsNeededDialog(false);
+    } else {
+      print('cannot use camera');
+    }
+    updateStatus(status);
+  }
+
+  Future<void> showLocationIsNeededDialog(bool completeyDenied) async {
+    var textDialog = "";
+    completeyDenied
+        ? textDialog =
+    'OCR needs to use your camera. If you press cancel you will not be able to use this feature.'
+        :
+    textDialog =
+    'OCR needs to use your camera. If you press cancel you will not be able to use this feature.';
+
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Camera access is needed'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                Text(textDialog),
+                Text('Would you like to allow using camera?'),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: Text('Allow'),
+              onPressed: () {
+                openAppSettings();
+              },
+            ),
+            TextButton(
+              child: Text('Cancel'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
 
 }
--- lib/Screens/OCRfeature/ocr_gallery/widget/text_recognition_widget.dart
+++ lib/Screens/OCRfeature/ocr_gallery/widget/text_recognition_widget.dart
@@ -5,6 +5,7 @@ import 'package:graduation_project/api/firebase_ml_api.dart';
 import 'package:graduation_project/Screens/OCRfeature/ocr_gallery/widget/text_area_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 import 'controls_widget.dart';
 
@@ -18,11 +19,27 @@ class TextRecognitionWidget extends StatefulWidget {
 }
 
 class _TextRecognitionWidgetState extends State<TextRecognitionWidget> {
+  PermissionStatus _status;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  updateStatus(PermissionStatus status) {
+    if (status != _status) {
+      setState(() {
+        _status = status;
+      });
+    }
+  }
+
   String text = '';
   File image;
 
   @override
-  Widget build(BuildContext context) => Expanded(
+  Widget build(BuildContext context) =>
+      Expanded(
         child: Column(
           children: [
             Expanded(child: buildImage()),
@@ -42,14 +59,18 @@ class _TextRecognitionWidgetState extends State<TextRecognitionWidget> {
       );
 
 
-  Widget buildImage() => Container(
+  Widget buildImage() =>
+      Container(
         child: image != null
             ? Image.file(image)
             : Icon(Icons.photo, size: 80, color: Colors.black),
       );
 
   Future pickImage() async {
+    Permission.storage.status.then((value) => updateStatus(value));
+    askLocationPermission();
     final file = await ImagePicker().getImage(source: ImageSource.gallery);
+    if(file!=null)
     setImage(File(file.path));
   }
 
@@ -88,5 +109,62 @@ class _TextRecognitionWidgetState extends State<TextRecognitionWidget> {
     setState(() {
       text = newText;
     });
+  }
+
+  void askLocationPermission() {
+    Permission.storage.request().then((status) => whenRequested(status));
+  }
+
+  whenRequested(PermissionStatus status) {
+    if (status.isDenied) {
+      showLocationIsNeededDialog(true);
+    } else if (Platform.isAndroid && status.isPermanentlyDenied) {
+      showLocationIsNeededDialog(false);
+    } else {
+      print('cannot use gallery');
+    }
+    updateStatus(status);
+  }
+
+  Future<void> showLocationIsNeededDialog(bool completeyDenied) async {
+    var textDialog = "";
+    completeyDenied
+        ? textDialog =
+    'Text recognition needs to access your media storage. If you press cancel you will not be able to use this feature.'
+        :
+    textDialog =
+    'text recognition needs to access your media library in order to pick an image. If you press cancel you will not be able to use this feature.';
+
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Access is needed'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                Text(textDialog),
+                Text('Would you like to allow accessing your media?'),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: Text('Allow'),
+              onPressed: () {
+                openAppSettings();
+              },
+            ),
+            TextButton(
+              child: Text('Cancel'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
   }
 }
